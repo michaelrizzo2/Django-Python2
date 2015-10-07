34e3507f4f9c051fd8839d7d62d1c00c5c02f481
michaelrizzo2 <m_rizzo@live.com>
2015-10-06 21:01:19 -0400

    Moved was published recently method from Choice class to Question class

--- my_first_site/mypoll/models.py
+++ my_first_site/mypoll/models.py
@@ -10,6 +10,9 @@ class Question(models.Model):
     def __unicode__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date>=timezone.now()-datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question=models.ForeignKey(Question)
     choice_text=models.CharField(max_length=200)
@@ -18,7 +21,5 @@ class Choice(models.Model):
     def __unicode__(self):
         return self.choice_text
 
-    def was_published_recently(self):
-        return self.pub_date>=timezone.now()-datetime.timedelta(days=1)
 
 
