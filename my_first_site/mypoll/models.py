1d181c839b7581a668e4dfd0aac74c3cf47209d4
michaelrizzo2 <m_rizzo@live.com>
2015-10-06 20:14:01 -0400

    Added was published recently method to Question class

--- my_first_site/mypoll/models.py
+++ my_first_site/mypoll/models.py
@@ -1,5 +1,6 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
@@ -16,4 +17,8 @@ class Choice(models.Model):
 
     def __unicode__(self):
         return self.choice_text
+
+    def was_published_recently(self):
+        return self.pub_date>=timezone.now()-datetime.timedelta(days=1)
+
 
