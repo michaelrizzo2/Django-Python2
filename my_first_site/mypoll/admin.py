534f6da5c8b91beb16e700b3dd7d19ac98f59c2a
michaelrizzo2 <m_rizzo@live.com>
2015-10-07 22:43:06 -0400

    Seperated Fields for Question and Date Information

--- my_first_site/mypoll/admin.py
+++ my_first_site/mypoll/admin.py
@@ -3,5 +3,6 @@ from .models import Question
 # Register your models here.
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields=['pub_date','question_text']
+    #fields=['pub_date','question_text']
+    fieldsets=[("Question",{'fields':['question_text']}),('Date information',{'fields':['pub_date']})]
 admin.site.register(Question,QuestionAdmin)
