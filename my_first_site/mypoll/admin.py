40f8a44754b8868de082dacfe22fccff75e1d8c2
michaelrizzo2 <m_rizzo@live.com>
2015-10-08 10:42:56 -0400

    Added filter and options for was_published_recently column

--- my_first_site/mypoll/admin.py
+++ my_first_site/mypoll/admin.py
@@ -8,6 +8,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     #fields=['pub_date','question_text']
     list_display=('question_text','pub_date','was_published_recently')
+    list_filter=['pub_date']
     fieldsets=[("Question",{'fields':['question_text']}),('Date information',{'fields':['pub_date'],'classes':['collapse']})]
     inlines=[ChoiceInline]
 admin.site.register(Question,QuestionAdmin)
