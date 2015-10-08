8faa274847d9ceae18fabc0052b0d4a8221eedbe
michaelrizzo2 <m_rizzo@live.com>
2015-10-07 22:46:39 -0400

    Added option to hide Date Information Panel

--- my_first_site/mypoll/admin.py
+++ my_first_site/mypoll/admin.py
@@ -4,5 +4,5 @@ from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
     #fields=['pub_date','question_text']
-    fieldsets=[("Question",{'fields':['question_text']}),('Date information',{'fields':['pub_date']})]
+    fieldsets=[("Question",{'fields':['question_text']}),('Date information',{'fields':['pub_date'],'classes':['collapse']})]
 admin.site.register(Question,QuestionAdmin)
