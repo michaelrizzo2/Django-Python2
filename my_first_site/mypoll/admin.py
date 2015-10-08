bb1464bdd9d1091caf31a7ff2b1e009ad98a4a87
michaelrizzo2 <m_rizzo@live.com>
2015-10-08 11:30:31 -0400

    added search field for question text

--- my_first_site/mypoll/admin.py
+++ my_first_site/mypoll/admin.py
@@ -9,6 +9,7 @@ class QuestionAdmin(admin.ModelAdmin):
     #fields=['pub_date','question_text']
     list_display=('question_text','pub_date','was_published_recently')
     list_filter=['pub_date']
+    search_fields=['question_text']
     fieldsets=[("Question",{'fields':['question_text']}),('Date information',{'fields':['pub_date'],'classes':['collapse']})]
     inlines=[ChoiceInline]
 admin.site.register(Question,QuestionAdmin)
