f929399fdacdda8b87b5e6f46e9cb415a99b9900
michaelrizzo2 <m_rizzo@live.com>
2015-10-10 19:09:13 -0400

    Added 3 new views and url links

--- my_first_site/mypoll/views.py
+++ my_first_site/mypoll/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World you are at the polls index")
+
+detail =lambda request,question_id : HttpResponse("You are looking at question %s" % question_id) 
+
+results=lambda request,question_id :HttpResponse("You are looking at the results of question %s" % question_id)
+
+vote=lambda request,question_id:HttpResponse("You are voting on question %s" % question_id) 
