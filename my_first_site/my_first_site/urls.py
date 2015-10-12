af5c0b4950ba3de85ac3e3ff3b5dd489404478d6
michaelrizzo2 <m_rizzo@live.com>
2015-10-12 19:29:44 -0400

    implemented namespaces for mypoll Django app

--- my_first_site/my_first_site/urls.py
+++ my_first_site/my_first_site/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^mypoll/',include('mypoll.urls')),
+    url(r'^mypoll/',include('mypoll.urls',namespace="mypoll")),
     url(r'^admin/', include(admin.site.urls)),
 ]
