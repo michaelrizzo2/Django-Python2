2e8c0eceafcdb0dd04659c490e80d7d79632bc21
michaelrizzo2 <m_rizzo@live.com>
2015-10-12 11:28:33 -0400

    Created view to show latest 5 questions

--- my_first_site/mypoll/views.py
+++ my_first_site/mypoll/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from .models import Question
+from django.template import RequestContext,loader
 # Create your views here.
 
+#def index(request):
+#    return HttpResponse("Hello World you are at the polls index")
 def index(request):
-    return HttpResponse("Hello World you are at the polls index")
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    template=loader.get_template('mypoll/index.html')
+    context=RequestContext(request,{'latest_question_list':latest_question_list,})
+    return HttpResponse(template.render(context))
 
 detail =lambda request,question_id : HttpResponse("You are looking at question %s" % question_id) 
 
