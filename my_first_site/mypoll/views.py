2e830798bb2c0e99269f3e299a01147a369fcc9c
michaelrizzo2 <m_rizzo@live.com>
2015-10-12 16:33:26 -0400

    Loaded index view with render instead of HttpResponse

--- my_first_site/mypoll/views.py
+++ my_first_site/mypoll/views.py
@@ -1,15 +1,21 @@
 from django.http import HttpResponse
 from .models import Question
-from django.template import RequestContext,loader
+from django.shortcuts import render 
+#from django.template import RequestContext,loader
 # Create your views here.
 
 #def index(request):
 #    return HttpResponse("Hello World you are at the polls index")
+#def index(request):
+#    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+#    template=loader.get_template('mypoll/index.html')
+#    context=RequestContext(request,{'latest_question_list':latest_question_list,})
+#    return HttpResponse(template.render(context))
+
 def index(request):
     latest_question_list=Question.objects.order_by('-pub_date')[:5]
-    template=loader.get_template('mypoll/index.html')
-    context=RequestContext(request,{'latest_question_list':latest_question_list,})
-    return HttpResponse(template.render(context))
+    context={'latest_question_list':latest_question_list}
+    return render(request,'mypoll/index.html',context)
 
 detail =lambda request,question_id : HttpResponse("You are looking at question %s" % question_id) 
 
