3444e5bbca80cf730e3f476c11aa15c192cd4273
michaelrizzo2 <m_rizzo@live.com>
2015-10-12 18:31:16 -0400

    Rewrote detail view using get_object_or_404() in Django

--- my_first_site/mypoll/views.py
+++ my_first_site/mypoll/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse,Http404
 from .models import Question
-from django.shortcuts import render 
+from django.shortcuts import render,get_object_or_404
 #from django.template import RequestContext,loader
 # Create your views here.
 
@@ -19,12 +19,17 @@ def index(request):
 
 #detail =lambda request,question_id : HttpResponse("You are looking at question %s" % question_id) 
 
+#def detail(request,question_id):
+#    try:
+#        question=Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request,'mypoll/details.html',{'question':question})
+
 def detail(request,question_id):
-    try:
-        question=Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question=get_object_or_404(Question,pk=question_id)
     return render(request,'mypoll/details.html',{'question':question})
+
 
 results=lambda request,question_id :HttpResponse("You are looking at the results of question %s" % question_id)
 
