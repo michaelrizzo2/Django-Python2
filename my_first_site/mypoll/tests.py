1390a88d36db7558f47912bd9b9d019bad7f4f94
michaelrizzo2 <m_rizzo@live.com>
2015-10-16 10:30:50 -0400

    Tested was published recently function for recent date issue

--- my_first_site/mypoll/tests.py
+++ my_first_site/mypoll/tests.py
@@ -18,3 +18,7 @@ class QuestionMethodTests(TestCase):
         old_question=Question(pub_date=time)
         self.assertEqual(old_question.was_published_recently(),False)
 
+    def test_was_published_recently_with_recent_question(self):
+        time=timezone.now()-datetime.timedelta(hours=1)
+        recent_question=Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(),True)
