8cbf03c51bb1bd4132c170995f34d3fcecb68bf0
michaelrizzo2 <m_rizzo@live.com>
2015-10-18 11:42:15 -0400

    Tested index view for no questions,test successful

--- my_first_site/mypoll/tests.py
+++ my_first_site/mypoll/tests.py
@@ -2,6 +2,7 @@ import datetime
 from django.utils import timezone
 from django.test import TestCase
 from .models import Question
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
 
@@ -22,3 +23,25 @@ class QuestionMethodTests(TestCase):
         time=timezone.now()-datetime.timedelta(hours=1)
         recent_question=Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(),True)
+
+def create_question(question_text,days):
+
+
+    """
+        Creates a question with the given `question_text` published the given
+        number of `days` offset to now (negative for questions published
+        in the past, positive for questions that have yet to be published).
+    """
+
+    time=timezone.now()+datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text,pub_date=time)
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        """
+        If no question exists , an appropiate message should be displayed
+        """
+        response=self.client.get(reverse('mypoll:index'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"No polls are available")
+        self.assertQuerysetEqual(response.context['latest_question_list'],[])
