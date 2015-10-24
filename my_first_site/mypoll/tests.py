b6ce5e267b50ab2bdb42707b73da0c9969adf983
michaelrizzo2 <m_rizzo@live.com>
2015-10-23 21:16:32 -0400

    tested detail view with past question,should get 200 which means the page exists,test successful

--- my_first_site/mypoll/tests.py
+++ my_first_site/mypoll/tests.py
@@ -104,3 +104,12 @@ class QuestionViewTests(TestCase):
          response=self.client.get(reverse("mypoll:detail",args=(future_question.id,)))
          self.assertEqual(response.status_code,404)
 
+    def test_detail_with_a_past_question(self):
+
+         """
+         The detail view of a question with a pub_date in the past should
+         display the question's text.
+         """
+         past_question=create_question(question_text="Past Question",days=-5)
+         response=self.client.get(reverse('mypoll:detail',args=(past_question.id,)))
+         self.assertContains(response,past_question.question_text,status_code=200)
