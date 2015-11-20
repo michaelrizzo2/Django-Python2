c1ca0007da71304c410555c5bbc5b8f782a94b95
michaelrizzo2 <m_rizzo@live.com>
2015-11-20 14:56:00 -0500

    added security to website,next will add login page

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'',include('myblog.urls')),
 ]
