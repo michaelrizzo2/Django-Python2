31c19afb93bf65b83772e5036fbb325a46f46e0b
michaelrizzo2 <m_rizzo@live.com>
2015-11-22 19:22:35 -0500

    added logout option to home page

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -19,5 +19,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',{'next_page':'/'}),
     url(r'',include('myblog.urls')),
 ]
